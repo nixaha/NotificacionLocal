aae7bf7aa30238b4c06672c9d134705aaca8e039
nixaha <nixaha_saucedo@hotmail.com>
2018-03-27 15:09:00 -0600

    Noti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    LocalNotifications,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { Platform } from 'ionic-angular/platform/platform';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, 
+    private plt: Platform, private localNotifications: LocalNotifications) {
 
+    this.plt.ready().then((rdy) =>{
+      this.localNotifications.on('click', (notification, state) =>{
+        let json = JSON.parse(notification.data);
+
+        let alert = this.alertCtrl.create({
+          title: notification.title,
+          subTitle: json.mydata
+        });
+        alert.present();
+      });
+    });
+  }
+
+  scheduleNotification(){
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Attention',
+      text:'Simons Notification',
+      at: new Date(new Date().getTime() + 5 * 1000),
+      data: { mydata: 'My hidden message this is'}
+    });
   }
 
 }
